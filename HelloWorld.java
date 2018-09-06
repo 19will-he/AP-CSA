939ee1c8025b180b6e094c6ee24d461bf1ec5721
19will-he <43040782+19will-he@users.noreply.github.com>
2018-09-06 08:37:07 -0700

    Added the HelloWorld src file

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+package com.williamheinsch.helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!\n");
+		System.out.println("This is line 3\n\n\n\n");
+		System.out.println("   /\\");
+		System.out.println("  /  \\");
+		System.out.println(" /    \\");
+		System.out.println("+------+");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("+------+");
+		System.out.println("|Gandhi|");
+		System.out.println("|India |");
+		System.out.println("+------+");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("+------+");
+		System.out.println("   /\\");
+		System.out.println("  /  \\");
+		System.out.println(" /    \\");
+	}
+}
